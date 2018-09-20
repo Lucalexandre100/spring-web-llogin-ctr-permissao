508f4097935faf00439ea31950cd8a4dae5f063b
lucalexandre100 <l.alexandre100@gmail.com>
2018-09-19 22:07:39 -0300

    Login com usuário padrao

---
+++ src/main/java/br/com/apl/web/PrimeiroProjSpring1Application.java
@@ -0,0 +1,12 @@
+package br.com.apl.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjSpring1Application.class, args);
+	}
+}
